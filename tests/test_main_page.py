bd864fccb19d1834750246a01996700ef0a045aa
FilippovAleksey1993 <aleksey.a.filippov@gmail.com>
2025-09-30 19:39:50 +0500

    tst

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -150,10 +150,13 @@ def test_popular_offers():
     print("Headers:", dict(resp.request.headers))
     print("Body:", resp.request.body)
 
-    print("=== RESPONSE ===")
-    print("Status Code:", resp.status_code)
-    print("Response Headers:", dict(resp.headers))
-    print("Response Body:", resp.text)
+
+
+
+    # print("=== RESPONSE ===")
+    # print("Status Code:", resp.status_code)
+    # print("Response Headers:", dict(resp.headers))
+    # print("Response Body:", resp.text)
 
     attach_reqres(resp)
     assert resp.status_code == 200, f"Expected 200, got {resp.status_code}. Response: {resp.text}"
