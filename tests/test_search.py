84e91e04e38c63a5171717dbcc9685682c28cd10
FilippovAleksey1993 <aleksey.a.filippov@gmail.com>
2025-10-10 18:30:37 +0500

    final

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,15 +2,15 @@ from utils.api_client import search_products
 from utils.helpers import get_first_product_name
 import allure
 
-@allure.feature("Поиск товаров")
-@allure.story("Поиск Varmilo")
+@allure.parent_suite('Поиск товаров')
+@allure.suite('Поиск Varmilo')
 def test_search_varmilo_status_code():
     response = search_products('Varmilo')
     with allure.step("200"):
          assert response.status_code == 200
 
-@allure.feature("Поиск товаров")
-@allure.story("Проверка результатов поиска")
+@allure.parent_suite('Поиск товаров')
+@allure.suite('Проверка результатов поиска')
 def test_search_varmilo_contains_term():
     response = search_products('Varmilo')
     data = response.json()
