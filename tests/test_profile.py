6a7c98467d7b30b88cc9e3a39a2b84f3937441ae
FilippovAleksey1993 <aleksey.a.filippov@gmail.com>
2025-10-10 18:37:52 +0500

    final

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -35,18 +35,3 @@ def test_phone_number_format():
         if data['profile'].get('phoneNumbers') and len(data['profile']['phoneNumbers']) > 0:
             phone_number = data['profile']['phoneNumbers'][0]
             assert validate_phone_number(phone_number)
-
-
-def test_profile_data_exists():
-    username = 998903574573
-    password = os.getenv('password')
-    login(username, password)
-
-    response = get_profile()
-    data = response.json()
-
-    if response.status_code == 200:
-        assert data['id'] is not None
-        assert data['username'] is not None
-        if 'firstName' in data['profile']:
-            assert data['profile']['firstName'] is not None
