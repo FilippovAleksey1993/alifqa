99da16dba040fe0d4098988ed81727530555c0b5
FilippovAleksey1993 <aleksey.a.filippov@gmail.com>
2025-10-10 18:29:46 +0500

    final

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,23 +1,23 @@
 from utils.api_client import get_products
 import allure
 
-@allure.feature("Главная страница")
-@allure.story("Получение списка товаров")
+@allure.parent_suite('Главная страница')
+@allure.suite('Получение списка товаров')
 def test_get_products_status_code():
     response = get_products()
     with allure.step("200"):
         assert response.status_code == 200
 
-@allure.feature("Главная страница")
-@allure.story("Проверка содержимого продуктов")
+@allure.parent_suite('Главная страница')
+@allure.suite('Проверка содержимого продуктов')
 def test_get_products_not_empty():
     response = get_products()
     data = response.json()
     with allure.step("Проверяем что список товаров не пустой"):
         assert len(data['variations']) > 0
 
-@allure.feature("Главная страница")
-@allure.story("Проверка структуры ответа")
+@allure.parent_suite('Главная страница')
+@allure.suite('Проверка структуры ответа')
 def test_get_products_has_correct_structure():
     response = get_products()
     data = response.json()
